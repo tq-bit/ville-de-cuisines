c9f4967a737e1aaef9595b8bffeb162f495796cc
q-bit <t.quante@outlook.com>
2022-04-19 18:03:42 +0200

    add sidebar

--- frontend/src/components/ui/navigation/AppSidebar.vue
+++ frontend/src/components/ui/navigation/AppSidebar.vue
@@ -1,7 +1,13 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
+import AppNavbarMain from './AppNavbarMain.vue';
+import AppNavbarSecondary from './AppNavbarSecondary.vue';
+
 withDefaults(defineProps<{ show: boolean }>(), {
 	show: false,
 });
+
+const isUserLoggedIn = ref<boolean>(true);
 </script>
 
 <template>
@@ -9,7 +15,17 @@ withDefaults(defineProps<{ show: boolean }>(), {
 		<aside
 			v-if="show"
 			class="fixed top-0 left-0 w-screen h-screen z-10 px-4 pt-20 bg-gray-50 dark:bg-gray-800 text-gray-800 dark:text-gray-50"
-		></aside>
+		>
+			<section class="md:hidden block">
+				<app-navbar-main direction="vertical" :is-user-logged-in="isUserLoggedIn"></app-navbar-main>
+			</section>
+			<section class="md:hidden block">
+				<app-navbar-secondary
+					direction="vertical"
+					:is-user-logged-in="isUserLoggedIn"
+				></app-navbar-secondary>
+			</section>
+		</aside>
 	</Transition>
 </template>
 
