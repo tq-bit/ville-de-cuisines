3a12cd1ed80d7aa2a7bf3ab3f6d6e605b6c89793
q-bit <t.quante@outlook.com>
2022-04-20 15:12:03 +0200

    add block variant to form card

--- frontend/src/components/form/AppFormCard.vue
+++ frontend/src/components/form/AppFormCard.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-defineProps<{ center?: boolean; title?: string }>();
+defineProps<{ center?: boolean; title?: string; block: boolean }>();
 </script>
 
 <template>
-	<div class="p-4 w-full max-w-md h-auto mx-auto border border-green-600 rounded bg-white shadow-md" :class="{ 'text-center': center }">
+	<div
+		class="p-4 w-full h-auto mx-auto border border-green-600 rounded bg-white shadow-md"
+		:class="{ 'text-center': center, 'max-w-md': !block }"
+	>
 		<div class="flex w-full items-center justify-center"></div>
 		<h3 class="text-3xl text-center mb-4">{{ title }}</h3>
 		<slot name="default" />
