55f5de3a5e52723cebd47d3ad344b68fb19e5f19
q-bit <t.quante@outlook.com>
2022-04-21 11:38:30 +0200

    feat: add preferences modal

---
+++ frontend/src/components/layout/AppScreenModal.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const emit = defineEmits(['click-blend'])
+</script>
+
+<template>
+	<section
+		@click.self="emit('click-blend')"
+		class="absolute top-0 left-0 h-screen w-screen bg-black bg-opacity-25 z-50"
+	>
+		<div class="relative">
+			<div
+				class="absolute top-24 left-6 right-6 md:left-24 md:right-24 lg:left-36 lg:right-36 xl:left-48 xl:right-48"
+			>
+				<slot />
+			</div>
+		</div>
+	</section>
+</template>
+
+<style scoped></style>
--- frontend/src/pages/Profile.vue
+++ frontend/src/pages/Profile.vue
@@ -3,6 +3,7 @@ import AppGrid from '../components/layout/AppGrid.vue';
 import AppButton from '../components/form/AppButton.vue';
 import AppImage from '../components/ui/AppImage.vue';
 import AppCard from '../components/form/AppCard.vue';
+import AppScreenModal from '../components/layout/AppScreenModal.vue';
 
 import { useRouter } from 'vue-router';
 import useSessionStore from '../store/sessionStore';
@@ -23,30 +24,37 @@ const logout = async () => {
 </script>
 
 <template>
-	<app-grid class="mt-4">
-		<template v-slot:left>
-			<app-card title="Your profile" block>
-				<app-image class="mb-4" :rounded="true" size="xsmall" :src="account.avatar"></app-image>
-
-				<app-button class="mb-4" @click="" block>Edit preferences</app-button>
-				<app-button class="mb-4" @click="logout" block>Log out</app-button>
+	<div class="mt-4">
+		<app-screen-modal @click-blend="">
+			<app-card block title="Settings">
+				Test
 			</app-card>
-		</template>
+		</app-screen-modal>
+		<app-grid >
+			<template v-slot:left>
+				<app-card title="Your profile" block>
+					<app-image class="mb-4" :rounded="true" size="xsmall" :src="account.avatar"></app-image>
 
-		<template v-slot:right
-			><app-card class="mb-4" title="Feed" block> </app-card>
+					<app-button class="mb-4" @click="" block>Edit preferences</app-button>
+					<app-button class="mb-4" @click="logout" block>Log out</app-button>
+				</app-card>
+			</template>
 
-			<app-card class="mb-4" title="First item" block>
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit consequatur culpa tempore
-				vero perspiciatis omnis quod quam ullam quibusdam alias dolor illum consequuntur laboriosam,
-				cum necessitatibus, nulla rem officia impedit!
-			</app-card>
+			<template v-slot:right
+				><app-card class="mb-4" title="Feed" block> </app-card>
 
-			<app-card class="mb-4" title="Second item" block>
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit consequatur culpa tempore
-				vero perspiciatis omnis quod quam ullam quibusdam alias dolor illum consequuntur laboriosam,
-				cum necessitatibus, nulla rem officia impedit!
-			</app-card>
-		</template>
-	</app-grid>
+				<app-card class="mb-4" title="First item" block>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit consequatur culpa tempore
+					vero perspiciatis omnis quod quam ullam quibusdam alias dolor illum consequuntur
+					laboriosam, cum necessitatibus, nulla rem officia impedit!
+				</app-card>
+
+				<app-card class="mb-4" title="Second item" block>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit consequatur culpa tempore
+					vero perspiciatis omnis quod quam ullam quibusdam alias dolor illum consequuntur
+					laboriosam, cum necessitatibus, nulla rem officia impedit!
+				</app-card>
+			</template>
+		</app-grid>
+	</div>
 </template>
