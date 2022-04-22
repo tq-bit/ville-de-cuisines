d104dcba473fe5e503afb0ed46b1047e10c77f0b
q-bit <t.quante@outlook.com>
2022-04-22 11:44:22 +0200

    ref: add variant template for grid

--- frontend/src/components/layout/content/AppGrid.vue
+++ frontend/src/components/layout/content/AppGrid.vue
@@ -1,14 +1,43 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    variant: 'sidebar-left' | 'sidebar-left-and-right';
+  }>(),
+  {
+    variant: 'sidebar-left',
+  },
+);
+</script>
 
 <template>
   <section
     class="grid grid-cols-12 gap-x-4 gap-y-8 px-4 w-full max-w-screen-xl mx-auto"
   >
-    <div class="col-span-12 sm:col-span-4 lg:col-span-3">
+    <div
+      class="col-span-12"
+      :class="{
+        'sm:col-span-4 lg:col-span-3': variant === 'sidebar-left',
+        'lg:col-span-3': variant === 'sidebar-left-and-right',
+      }"
+    >
       <slot name="left" />
     </div>
-    <div class="col-span-12 sm:col-span-8 lg:col-span-9">
+    <div
+      class="col-span-12"
+      :class="{
+        'sm:col-span-8 lg:col-span-9': variant === 'sidebar-left',
+        'lg:col-span-6': variant === 'sidebar-left-and-right',
+      }"
+    >
       <slot name="default" />
+    </div>
+    <div
+      class="absolute top-0 left-0 w-full h-full"
+      :class="{
+        'lg:col-span-3': variant === 'sidebar-left-and-right',
+      }"
+    >
+      <slot name="right" />
     </div>
   </section>
 </template>
