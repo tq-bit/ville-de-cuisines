0b1384aa2872cdfb29b9b4181414309391244d7c
q-bit <t.quante@outlook.com>
2022-04-25 18:48:42 +0200

    style: screen modal

--- frontend/src/components/layout/AppScreenModal.vue
+++ frontend/src/components/layout/AppScreenModal.vue
@@ -5,12 +5,12 @@ const emit = defineEmits(['click-blend']);
 <template>
   <section
     @click.self="emit('click-blend')"
-    class="absolute top-0 left-0 h-full w-screen bg-black bg-opacity-50 z-50 px-2 md:px-6"
+    class="fixed top-0 left-0 h-full w-full bg-black bg-opacity-50 z-50"
   >
-    <div class="relative">
-      <div class="absolute top-2 md:top-24 left-0 right-0 max-w-xl mx-auto">
-        <slot />
-      </div>
+    <div
+      class="absolute md:top-24 max-h-screen overflow-auto left-0 right-0 max-w-xl mx-auto"
+    >
+      <slot />
     </div>
   </section>
 </template>
