17e517c7c769ff6af698c84ea18f67a61272c25f
q-bit <t.quante@outlook.com>
2022-05-02 20:12:20 +0200

    adjust lazy search

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -7,4 +7,4 @@ export const USER_COLLECTION_ID = '625d483a10f5de955b29';
 export const INGREDIENTS_COLLECTION_ID = '625d3dbd6d3d310cd0ae';
 export const RECIPES_COLLECTION_ID = '625d4675ce288a1398b2';
 
-export const LAZY_SEARCH_TIMEOUT = 1000;
+export const LAZY_SEARCH_TIMEOUT = 500;
