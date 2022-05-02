f24f4607dfbe166f990f025df97b11f682663ed1
q-bit <t.quante@outlook.com>
2022-05-02 16:20:33 +0200

    feat: add hero to starting page

---
+++ frontend/src/components/ui/AppHero.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import AppGrid from '../layout/content/AppGrid.vue';
+import ImgCookingHero from '../img/imgCookingHero.vue';
+import AppButton from '../form/AppButton.vue';
+
+const emit = defineEmits(['clickGetStarted']);
+</script>
+
+<template>
+  <app-grid variant="equal">
+    <template v-slot:left>
+      <section>
+        <img-cooking-hero></img-cooking-hero>
+      </section>
+    </template>
+
+    <template v-slot:default>
+      <section
+        class="mt-12 flex h-full w-full items-center justify-center md:mt-0"
+      >
+        <div class="text-center">
+          <h1 class="mb-4 text-2xl md:text-4xl">
+            iCuisine - Your kitchen's digital twin
+          </h1>
+          <p class="text-md mb-4 md:text-lg">
+            Create and manage your favorite recipes - then share them with the
+            world.
+          </p>
+          <app-button @click="emit('clickGetStarted')">Get started</app-button>
+        </div>
+      </section>
+    </template>
+  </app-grid>
+</template>
+
+<style scoped></style>
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,35 +1,17 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import AppContainer from '../components/layout/content/AppContainer.vue';
-import AppInput from '../components/form/AppInput.vue';
-import iUser from '../components/icons/iUser.vue';
-import iLock from '../components/icons/iLock.vue';
-
-let passwordFieldType = ref<'text' | 'password'>('password');
-
-const togglePasswordInputType = () => {
-  passwordFieldType.value =
-    passwordFieldType.value === 'text' ? 'password' : 'text';
-};
+import AppHero from '../components/ui/AppHero.vue';
+import { useRouter } from 'vue-router';
+const router = useRouter();
 </script>
 
 <template>
   <app-container page>
-    <app-input label="Please ne">
-      <template v-slot:icon>
-        <i-user></i-user>
-      </template>
-    </app-input>
-    <app-input
-      :type="passwordFieldType"
-      label-prefix="Please enter your "
-      label="Password"
-      @click-icon="togglePasswordInputType"
-    >
-      <template v-slot:icon>
-        <i-lock class="cursor-pointer"></i-lock>
-      </template>
-    </app-input>
+    <app-hero
+      class="mt-12"
+      @clickGetStarted="router.push({ path: '/login' })"
+    ></app-hero>
   </app-container>
 </template>
 
