9de297c6129c8fc55c7d330c51442ba1524bd5c5
q-bit <t.quante@outlook.com>
2022-05-04 09:44:03 +0200

    fix: adjust style for ingredient list

--- frontend/src/components/lists/ingredients/AppIngredientList.vue
+++ frontend/src/components/lists/ingredients/AppIngredientList.vue
@@ -1,23 +1,37 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import { Ingredient } from '../../../@types/commons';
 import AppIngredientItem from './AppIngredientItem.vue';
 
-defineProps<{ editable: boolean; ingredients: Ingredient[] }>();
+const props = defineProps<{ editable: boolean; ingredients: Ingredient[] }>();
 const emit = defineEmits<{
   (event: 'removeIngredient', ingredient: Ingredient): void;
 }>();
+
+const hasIngredients = computed(() => props.ingredients.length > 0);
 </script>
 
 <template>
-  <ul
-    class="mb-2 flex w-full flex-wrap rounded border-l-4 py-2 pl-2 dark:border-gray-600 dark:bg-gray-800"
+  <section
+    class="mb-2 flex rounded border-l-4 bg-gray-50 py-2 pl-2 dark:border-gray-700 dark:bg-gray-800"
+    :class="{
+      'border-green-600 dark:border-green-600': hasIngredients,
+    }"
   >
-    <app-ingredient-item
-      v-for="ingredient in ingredients"
-      :key="ingredient.$id"
-      :ingredient="ingredient"
-      :editable="editable"
-      @click-remove="emit('removeIngredient', ingredient)"
-    ></app-ingredient-item>
-  </ul>
+    <p
+      class="mx-auto py-4 text-center font-semibold text-green-600"
+      v-if="!hasIngredients"
+    >
+      No ingredients added yet
+    </p>
+    <ul class="w-full" v-else>
+      <app-ingredient-item
+        v-for="ingredient in ingredients"
+        :key="ingredient.$id"
+        :ingredient="ingredient"
+        :editable="editable"
+        @click-remove="emit('removeIngredient', ingredient)"
+      ></app-ingredient-item>
+    </ul>
+  </section>
 </template>
