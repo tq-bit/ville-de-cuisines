a36ff2c2003b4be78153b32dd5bd2e8096cc8b0c
q-bit <t.quante@outlook.com>
2022-05-04 11:14:29 +0200

    add submitted by fun

--- frontend/src/pages/recipe/index.vue
+++ frontend/src/pages/recipe/index.vue
@@ -1,17 +1,32 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import { Recipe } from '../../@types/commons';
 import AppContainer from '../../components/layout/content/AppContainer.vue';
 import AppGrid from '../../components/layout/content/AppGrid.vue';
 import AppImage from '../../components/ui/AppImage.vue';
 import useRecipeStore from '../../store/recipeStore';
+import useActiveUserStore from '../../store/activeUserStore';
 
+// Router logic
 const router = useRouter();
 const recipeId = router.currentRoute.value.params.recipeId as string;
+
+// User logic
+const activeUserStore = useActiveUserStore();
+const submittedBy = computed(() => {
+  const activeUserSubmitted =
+    activeUserStore.user.$id === localRecipe.value?.user_id;
+  if (activeUserSubmitted) {
+    return 'you';
+  } else {
+    return localRecipe.value?.username;
+  }
+});
+
+// Recipe Logic
 const recipeStore = useRecipeStore();
 const localRecipe = ref<Recipe>();
-
 const setLocalRecipe = async () => {
   const [response, error] = await recipeStore.fetchRecipeById(recipeId);
   if (error) {
@@ -19,7 +34,6 @@ const setLocalRecipe = async () => {
   }
   localRecipe.value = response as Recipe;
 };
-
 onMounted(async () => await setLocalRecipe());
 </script>
 
@@ -35,7 +49,14 @@ onMounted(async () => await setLocalRecipe());
 
       <template v-slot:default>
         <h1 class="mb-4 text-3xl">{{ localRecipe?.name }}</h1>
-        <p>Submitted by {{ localRecipe?.username }}</p>
+        <p>
+          Submitted by
+          <router-link
+            class="font-semibold"
+            :to="`/user/${localRecipe?.user_id}`"
+            >{{ submittedBy }}</router-link
+          >
+        </p>
       </template>
     </app-grid>
   </app-container>
