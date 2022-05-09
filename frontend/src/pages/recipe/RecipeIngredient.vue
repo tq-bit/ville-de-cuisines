20d1f5da1c4c1911d66c21235bd36f31059c87cb
q-bit <t.quante@outlook.com>
2022-05-09 15:49:11 +0200

    chore: rename slogan

--- frontend/src/pages/recipe/RecipeIngredient.vue
+++ frontend/src/pages/recipe/RecipeIngredient.vue
@@ -74,7 +74,7 @@ onMounted(async () => {
 
       <template v-slot:default>
         <h1 class="mb-4 text-3xl md:text-4xl">
-          Recipes in which {{ localIngredient?.name }} is used:
+          Recipes where "{{ localIngredient?.name }}" is used:
         </h1>
         <app-gallery
           :items="localRecipesByIngredient"
