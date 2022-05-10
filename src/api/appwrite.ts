495422fe7e44306742d0f8797f9a599ad437d239
q-bit <t.quante@outlook.com>
2022-05-10 13:22:02 +0200

    dev: add conditional URL Settings

--- src/api/appwrite.ts
+++ src/api/appwrite.ts
@@ -2,8 +2,11 @@ import { Appwrite } from 'appwrite';
 
 const client = new Appwrite();
 
+const PROD_URL = 'https://kitchen.q-bit.me/v1'
+const DEV_URL = 'http://localhost/v1'
+
 client
-  .setEndpoint('https://kitchen.q-bit.me/v1')
+  .setEndpoint(import.meta.env.DEV ? DEV_URL : PROD_URL)
   .setProject('625ea6425efdf4cc9a46');
 
 export default client;
