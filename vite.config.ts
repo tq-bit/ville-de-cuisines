a986beb01f09da1623b443f2a43af12dec8748a3
q-bit <t.quante@outlook.com>
2022-05-10 18:37:45 +0200

    ref: add relative import path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
+// @ts-ignore
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), Components()],
+  resolve: {
+    alias: {
+      // @ts-ignore
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
