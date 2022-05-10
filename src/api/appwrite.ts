063b75788713f14d3ba45f62d7db1d07059daaa1
q-bit <t.quante@outlook.com>
2022-05-10 13:24:52 +0200

    adjust URL settings

--- src/api/appwrite.ts
+++ src/api/appwrite.ts
@@ -3,8 +3,11 @@ import { v4 as uuid } from 'uuid';
 import { AppServerResponseOrError } from '../@types';
 
 const appwriteClient = new Appwrite();
+const PROD_URL = 'https://kitchen.q-bit.me/v1'
+const DEV_URL = 'http://localhost/v1'
+
 appwriteClient
-  .setEndpoint('https://kitchen.q-bit.me/v1')
+  .setEndpoint(import.meta.env.DEV ? DEV_URL : PROD_URL)
   .setProject('625ea6425efdf4cc9a46');
 export { appwriteClient };
 
