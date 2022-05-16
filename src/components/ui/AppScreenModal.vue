878ede33207c59327166a6c79106f91a2f188147
q-bit <t.quante@outlook.com>
2022-05-16 14:21:11 +0200

    fix: position of modal in mobile

--- src/components/ui/AppScreenModal.vue
+++ src/components/ui/AppScreenModal.vue
@@ -8,7 +8,7 @@ const emit = defineEmits(['click-blend']);
     class="fixed top-0 left-0 z-50 h-full w-full bg-black bg-opacity-50"
   >
     <div
-      class="absolute left-0 right-0 mx-auto max-h-screen max-w-xl overflow-auto md:top-24"
+      class="absolute left-4 right-4 top-12 mx-auto h-fit max-h-screen max-w-xl overflow-auto md:top-24"
     >
       <slot />
     </div>
