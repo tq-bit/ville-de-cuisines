f872c1045d2c7b8851af489c247d620930f4de0c
q-bit <t.quante@outlook.com>
2022-05-17 13:45:46 +0200

    fix: hover in light theme

--- src/components/layout/navbar/AppRouterLink.vue
+++ src/components/layout/navbar/AppRouterLink.vue
@@ -14,7 +14,7 @@ withDefaults(
   <router-link
     class="w-fit rounded py-2 px-3 text-sm font-semibold"
     :class="{
-      'text-green-600 transition-colors hover:bg-gray-700':
+      'text-green-600 transition-colors hover:bg-gray-100 hover:dark:bg-gray-700':
         variant === 'default',
       'bg-gradient-to-bl from-green-400 to-green-500  text-gray-50 dark:from-green-600 dark:to-green-700':
         variant === 'button',
