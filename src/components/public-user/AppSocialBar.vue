684f08ec4d12507eb068a6442d6b7019ff90795b
q-bit <t.quante@outlook.com>
2022-05-17 09:33:56 +0200

    ref: abstract app social bar

---
+++ src/components/public-user/AppSocialBar.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { AppPublicUser } from '@/@types';
+defineProps<{
+  publicUser: AppPublicUser;
+}>();
+</script>
+
+<template>
+  <div>
+    <a
+      v-if="publicUser.facebook_url"
+      :href="publicUser.facebook_url"
+      target="_blank"
+      class="mx-2 inline-block"
+    >
+      <i-facebook></i-facebook>
+    </a>
+
+    <a
+      v-if="publicUser.instagram_url"
+      :href="publicUser.instagram_url"
+      target="_blank"
+      class="mx-2 inline-block"
+    >
+      <i-instagram></i-instagram>
+    </a>
+    <a
+      v-if="publicUser.pinterest_url"
+      :href="publicUser.pinterest_url"
+      target="_blank"
+      class="mx-2 inline-block"
+    >
+      <i-pinterest></i-pinterest>
+    </a>
+  </div>
+</template>
+
+<style scoped></style>
