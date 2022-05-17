9666e9fd7d0609db22d78d594790dbc618a5ac5e
q-bit <t.quante@outlook.com>
2022-05-17 14:13:56 +0200

    feat: add app tile element

---
+++ src/components/ui/AppTile.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  color: 'sky' | 'amber' | 'rose';
+}>();
+</script>
+
+<template>
+  <div
+    class="mx-auto h-auto w-full max-w-sm rounded-md border bg-gradient-to-bl p-4"
+    :class="{
+      'border-sky-200 from-sky-300 to-sky-400 dark:border-sky-900  dark:from-sky-600 dark:to-sky-800':
+        color === 'sky',
+      'border-amber-200 from-amber-300 to-amber-400  dark:border-amber-900  dark:from-amber-600 dark:to-amber-800':
+        color === 'amber',
+      'border-rose-200 from-rose-300 to-rose-400  dark:border-rose-900  dark:from-rose-600 dark:to-rose-800':
+        color === 'rose',
+    }"
+  >
+    <h3>{{ title }}</h3>
+    <main class="flex">
+      <div class="w-5/6">
+        <slot />
+      </div>
+      <div class="w-1/6">
+        <slot name="icon" />
+      </div>
+    </main>
+  </div>
+</template>
