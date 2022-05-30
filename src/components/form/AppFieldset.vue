eefc60938b4db04ae093ac8bb4eda792d103bb64
q-bit <t.quante@outlook.com>
2022-05-30 09:53:41 +0200

    feat: add fieldset component

---
+++ src/components/form/AppFieldset.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  legend: string;
+}>();
+</script>
+
+<template>
+  <fieldset class="border-green-600">
+    <legend class="font-semibold text-green-600 underline">
+      {{ legend }}
+    </legend>
+    <slot />
+  </fieldset>
+</template>
+
+<style scoped></style>
